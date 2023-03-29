348eb5349c7b26dcd3a2cc696c26cba688ee93c0
YeevStarr <olufemiyvonne@gmail.com>
2023-03-29 23:34:32 +0100

    Outputting characters with putchar().

---
+++ 10-main.c
@@ -0,0 +1,17 @@
+// Outputting characters with putchar
+#include <stdio.h>
+
+int main()
+{
+	putchar(72);
+	putchar(105);
+	putchar(32);
+	putchar(116);
+	putchar(104);
+	putchar(101);
+	putchar(114);
+	putchar(101);
+	putchar(33);
+	putchar(10);
+	return 0;
+}
