cacada863eaecd7d89e7ce4ecba632f0ebc26ac8
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-05 14:34:20 +0100

    Dealing with numbers

---
+++ 14-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+/*	// a program that print all number less than 20 in reverse order
+	int i = 20;
+
+	while (i > 0)
+	{
+		printf("%d ", i);
+		i--; // decrement by 1
+	} */
+
+/*	// print all numbers less than 100 in reverse order jumping 4 numbers
+	int i = 100;
+
+	while (i > 0)
+	{
+		printf("%d ", i);
+		i-=5; //decrement by 5... i = i - 5
+	} */
+
+	// multiples of 3 under 120 in reverse order
+	int i = 120;
+
+	printf("Multiples of 3 under 120 in reverse order:\n");
+	while (i >= 3)
+	{
+		if (i % 3 == 0)
+		{
+		printf("%d ", i);
+		}
+		i--;
+	}
+
+	printf("\n");
+	return 0;
+}
