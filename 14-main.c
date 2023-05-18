ce8d1cd6fae1abbddf11f79a34b19aa6948e229b
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-18 22:58:46 +0100

    A program that prints the multiples of 3 under 120 in reverse order.

--- 14-main.c
+++ 14-main.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 
-int main()
+/**
+  * main - Entry point
+  *
+  * multiples of 3 under 120 in reverse order
+  *
+  * Return: 0
+  */
+
+int main(void)
 {
-/*	// a program that print all number less than 20 in reverse order
-	int i = 20;
+	/**
+	  *a program that print all number less than 20 in reverse order
+	  *int i = 20;
+	  *
+	  *while (i > 0)
+	  *{
+	  *
+	  *printf("%d ", i);
+	  *i--;
+	  *}
+	  */
 
-	while (i > 0)
-	{
-		printf("%d ", i);
-		i--; // decrement by 1
-	} */
+	/**
+	  *print all numbers less than 100 in reverse order jumping 4 numbers
+	  *int i = 100;
+	  *
+	  *while (i > 0)
+	  *{
+	  *printf("%d ", i);
+	  *i-=5;	decrement by 5... i = i - 5
+	  *}
+	  */
 
-/*	// print all numbers less than 100 in reverse order jumping 4 numbers
-	int i = 100;
-
-	while (i > 0)
-	{
-		printf("%d ", i);
-		i-=5; //decrement by 5... i = i - 5
-	} */
-
-	// multiples of 3 under 120 in reverse order
 	int i = 120;
 
 	printf("Multiples of 3 under 120 in reverse order:\n");
@@ -28,11 +40,11 @@ int main()
 	{
 		if (i % 3 == 0)
 		{
-		printf("%d ", i);
+			printf("%d ", i);
 		}
 		i--;
 	}
-
 	printf("\n");
-	return 0;
+
+	return (0);
 }
