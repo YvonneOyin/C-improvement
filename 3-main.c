a0a1ab5fa24810b96ffe3454f1d14b97ef9d1df9
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-18 18:42:32 +0100

    A program that stores the address of a variable into a pointer

--- 3-main.c
+++ 3-main.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-  int n;
-  int *p;
+	int n;
+	int *p;
 
-  n = 98;
-  p = &n;
-  printf("Address of 'n': %p\n", &n);
-  printf("Value of 'p': %p\n", p);
-  return (0);
+	n = 98;
+	p = &n;
+	printf("Address of 'n': %p\n", &n);
+	printf("Value of 'p': %p\n", p);
+
+	return (0);
 }
