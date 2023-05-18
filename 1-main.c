bb1e145e07bd018099e36ee4e9add3d67a747fca
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-18 18:38:42 +0100

    Print address of variables

--- 1-main.c
+++ 1-main.c
@@ -7,10 +7,11 @@
  */
 int main(void)
 {
-   char c;
-   int n;
+	char c;
+	int n;
 
-   printf("Address of variable 'c': %p\n", &c);
-   printf("Address of variable 'n': %p\n", &n);
-   return (0);
+	printf("Address of variable 'c': %p\n", &c);
+	printf("Address of variable 'n': %p\n", &n);
+
+	return (0);
 }
