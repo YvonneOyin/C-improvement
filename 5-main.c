b6d486fa58b4408b38da53eb3c766ff4f5273e79
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-18 19:10:17 +0100

    A program that adds two integers and returns the result

--- 5-main.c
+++ 5-main.c
@@ -1,20 +1,33 @@
-// Making function call
-/* Calculate an addition and print out the result */
 #include <stdio.h>
-/* This function adds two integer and returns the result */
+
+/**
+ * integer_add - print result
+ *
+ * main - Entry point
+ *
+ * This function adds two integer and returns the result
+ *
+ * Return: result in integer_add
+ *
+ * @x: first variable
+ *
+ * @y: second variable
+ */
 
 int integer_add(int x, int y)
 {
 	int result;
+
 	result = x + y;
-	return result;
+	return (result);
 }
 
-int main()
+int main(void)
 {
 	int sum;
 
 	sum = integer_add(8, 13);
 	printf("The addition of 8 and 13 is %d.\n", sum);
-	return 0;
+
+	return (0);
 }
