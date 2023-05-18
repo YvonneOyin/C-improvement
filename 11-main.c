7c7f236e0e34b891d4cbbcd8c77e6462056fcc66
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-18 22:30:59 +0100

    A program that prints out all numbers less than 20 indicating which is even and which is odd

--- 11-main.c
+++ 11-main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-// Print out all numbers less than 20 indicating if even or odd
+/**
+  * main - Entry point
+  *
+  * Print out all numbers less than 20 indicating if even or odd
+  *
+  * Return: 0
+  */
 
-int main()
+int main(void)
 {
 	int n = 1;
 
 	while (n < 21)
 	{
-		if (n%2==0)
+		if (n % 2 == 0)
 		{
 			printf("%d is even\n", n);
 		}
@@ -16,10 +22,9 @@ int main()
 		{
 			printf("%d is odd\n", n);
 		}
-
-		// increment by 1
-		// n = n + 1
+		/* increment by 1 */
+		/* n = n + 1 */
 		n++;
 	}
-	return 0;
+	return (0);
 }
