16d75892d743e18f5ed56c00756f5d7c3cea29e0
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-18 19:31:17 +0100

    Reading input by calling getchar()

--- 8-main.c
+++ 8-main.c
@@ -1,14 +1,19 @@
-// Reading input by calling getchar()
+/**
+  * main - Entry point
+  * Reading input by calling getchar()
+  * Return: 0
+  */
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	int ch1, ch2;
 
 	printf("Please type in two characters together:\n");
-	ch1 = getc( stdin );
-	ch2 = getchar( );
+	ch1 = getc(stdin);
+	ch2 = getchar();
 	printf("The first character entered is: %c\n", ch1);
 	printf("The second character entered is: %c\n", ch2);
+
 	return (0);
 }
