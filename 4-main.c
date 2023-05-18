461d993058a1612d4b3af0ff85e663b954495d53
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-18 18:45:40 +0100

    Dereferencing pointers.

--- 4-main.c
+++ 4-main.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-   int n;
-   int *p;
+	int n;
+	int *p;
 
-   n = 98;
-   p = &n;
-   printf("Value of 'n': %d\n", n);
-   printf("Address of 'n': %p\n", &n);
-   printf("Value of 'p': %p\n", p);
-   *p = 530;
-   printf("Value of 'n': %d\n", n);
-   return (0);
+	n = 98;
+	p = &n;
+	printf("Value of 'n': %d\n", n);
+	printf("Address of 'n': %p\n", &n);
+	printf("Value of 'p': %p\n", p);
+
+	*p = 530;
+	printf("Value of 'n': %d\n", n);
+	return (0);
 }
