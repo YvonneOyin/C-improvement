fb71df629381433a83348bdd954849997fd3f67e
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-18 10:41:40 +0100

    A program that prints all odd numbers less than 50

--- 15-main.c
+++ 15-main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int i = 1;
 
-	// A program that prints all odd numbers less than 50
+	/*A program that prints all odd numbers less than 50*/
 	printf("All odd numbers less than 50:\n");
 	while (i <= 50)
 	{
-		//if (i % 2 != 0)
+		/*if (i % 2 != 0)*/
 		if (i % 2 == 1)
 		{
 			printf("%d ", i);
@@ -17,5 +22,5 @@ int main()
 		i++;
 	}
 	printf("\n");
-	return 0;
+	return (0);
 }
