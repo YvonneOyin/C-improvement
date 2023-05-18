c910f91827be35610a710824146f5231fb2efdb5
YvonneOyin <olufemioyindamola37@gmail.com>
2023-05-18 19:21:46 +0100

    A program that multiplies 2 integers

--- 6-main.c
+++ 6-main.c
@@ -1,20 +1,30 @@
-// Making function call
-/* Calculate a multiplication and print out the result */
 #include <stdio.h>
-/* This function multiplies two integer and returns the result */
+
+/**
+  * integer_multi - multiplies 2 integer
+  * main - Entry point
+  * @x: first variable
+  * @y: second variable
+  * Making function call
+  * Calculate a multiplication and print out the result
+  * This function multiplies two integer and returns the result
+  * Return: result in integer_multi
+  * Return 0
+  */
 
 int integer_multi(int x, int y)
 {
 	int result;
+
 	result = x * y;
-	return result;
+	return (result);
 }
 
-int main()
+int main(void)
 {
 	int total;
 
 	total = integer_multi(8, 13);
 	printf("The multiplication of 8 and 13 is %d.\n", total);
-	return 0;
+	return (0);
 }
